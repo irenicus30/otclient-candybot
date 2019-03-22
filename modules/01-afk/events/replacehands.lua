22dfa4e946ac5ccee19ec846a942f07672872852
irenicus30 <irenicus30@gmail.com>
2019-03-22 23:18:17 +0100

    add refill ammunition

--- modules/01-afk/events/replacehands.lua
+++ modules/01-afk/events/replacehands.lua
@@ -11,23 +11,33 @@ function AutoReplaceHands.Event(event)
     local player = g_game.getLocalPlayer()
 
     local selectedItem = AfkModule.getPanel():getChildById('ItemToReplace'):getItem():getId()
-    local item = player:getItem(selectedItem)
+    local item = player:getItemInContainers(selectedItem)
     
     local hand = InventorySlotOther
     if AfkModule.getPanel():getChildById('AutoReplaceWeaponSelect'):getText() == "Left Hand" then
       hand = InventorySlotLeft
-    else
+    elseif AfkModule.getPanel():getChildById('AutoReplaceWeaponSelect'):getText() == "Right Hand" then
       hand = InventorySlotRight
+    else
+      hand = InventorySlotAmmo
     end
     local handPos = {['x'] = 65535, ['y'] = hand, ['z'] = 0}
 
     local handItem = player:getInventoryItem(hand)
-    if handItem and handItem:getCount() > 3 then
+    if hand~=InventorySlotAmmo and handItem and handItem:getCount() > 3 then
+      return 10000
+    end
+    if hand==InventorySlotAmmo and handItem and handItem:getCount() > 50 then
       return 10000
     end
 
-    if item and (not handItem or handItem:getId() ~= item:getId()) then
-      g_game.move(item, handPos, item:getCount())
+    if item then --and (not handItem or handItem:getId() ~= item:getId()) then
+      amountToMove = item:getCount()
+      if handItem~=nil and item:getCount() + handItem:getCount() > 100 then
+        amountToMove = 100 - handItem:getCount()
+      end
+      BotLogger.debug("AutoReplaceHands.Event: moving amount " .. tostring(amountToMove))
+      g_game.move(item, handPos, amountToMove)
     end
   end
 
